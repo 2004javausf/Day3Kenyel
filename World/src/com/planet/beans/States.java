058b5d941028d6e3cdef68a9e59150478e2676a2
kib4885 <kib4885@gmail.com>
2020-04-15 16:13:27 -0400

    first

---
+++ World/src/com/planet/beans/States.java
@@ -0,0 +1,35 @@
+package com.planet.beans;
+
+public class States {
+	private String name;
+	private String nickname;
+	private int size;
+	
+	public String getName (String name) {
+		return name;
+	}
+	
+	public void setName (String name) {
+		this.name=name;
+	}
+	
+	public String getNickname (String nickname) {
+		return nickname;
+	}
+	
+	public void setNickname (String nickname) {
+		this.nickname=nickname;
+	}
+		
+	public int getSize() {
+		return size;
+				
+	}
+	public void setSize() {
+		this.size=size;
+	}
+	
+	public String toString() {
+		return "Countries [name="+name +", nickname="+nickname + ", size=" + size +"]";
+	}
+}
